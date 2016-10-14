b9bfb36e500f3114dd8773b2baceced5940a10af
tyl5566bryan <ty014@ie.cuhk.edu.hk>
2016-10-14 17:58:40 +0800

    add both generation and reconstruction

--- lua/generateVoxel_VAE.lua
+++ lua/generateVoxel_VAE.lua
@@ -14,6 +14,8 @@ opt = {
     gpu = 1,               -- gpu mode. 0 = CPU, 1 = GPU
     nz = 100,
     data_file = '/home/yltian/3D/Data/val_HDF5/04379243.h5',
+    encoder = './checkpoints/3DShape_25_Encoder.t7',
+    decoder = './checkpoints/3DShape_25_Decoder.t7',
 }
 
 if opt.gpu > 0 then
@@ -31,13 +33,24 @@ local data = h5_file:read('/data'):all()
 
 local inputs = torch.Tensor(data:size(1), 1, 32, 32, 32):fill(0)
 inputs[{{},{},{2,31},{2,31},{2,31}}]:copy(data)
+local noise  = torch.Tensor(opt.batch_size, opt.nz)
+noise:normal(0, 1)
 
 local model = torch.load(opt.net)
+local encoder = torch.load(opt.encoder)
+local decoder = torch.load(opt.decoder)
 
 if opt.gpu > 0 then
     model:cuda()
+    encoder:cuda()
+    decoder:cuda()
+    
     cudnn.convert(model, cudnn)
+    cudnn.convert(encoder, cudnn)
+    cudnn.convert(decoder, cudnn)
+    
     inputs = inputs:cuda()
+    noise = noise:cuda()
 end
 
 -- sample output and save
@@ -48,7 +61,21 @@ reconstruction, mean, log_var = unpack(model:forward(inputs))
 local res = torch.Tensor(reconstruction:size())
 res:copy(reconstruction)
 
+local e_mean, e_log_var, d_reconstruction
+e_mean, e_log_var = unpack(encoder:forward(inputs))
+d_reconstruction = decoder:forward(e_mean)
+
+local res2 = torch.Tensor(d_reconstruction:size())
+res2:copy(d_reconstruction)
+
+local generation = decoder:forward(noise)
+local res3 = torch.Tensor(generation:size())
+res3:copy(generation)
+
+local mydata = torch.cat(res, res2, 1)
+mydata = torch.cat(mydata, res3, 1)
+
 local myFile = hdf5.open(opt.name, 'w')
-myFile:write('data', res)
+myFile:write('data', mydata)
 myFile:close()
 
--- matlab/showHDF5.m
+++ matlab/showHDF5.m
@@ -1,50 +1,62 @@
 %% show generating results of GAN
-% clear;close all; clc;
+clear;close all; clc;
 
 file = '/home/yltian/3D/Code/My3DCode/lua/3Dgeneration_table_iter25.h5';
 data = h5read(file, '/data');
 data = permute(data, [5, 4, 3, 2, 1]);
 data = squeeze(data);
 
-i = 24;
-sample = data(i, 2:31, 2:31, 2:31);
-sample = squeeze(sample);
-ind1 = find(sample > 0.5); ind2 = find(sample <= 0.5);
-sample(ind1) = 1; sample(ind2) = 0;
-sample = double(sample);
+for i = 1:size(data,1)
+    
+    sample = data(i, 2:31, 2:31, 2:31);
+    sample = squeeze(sample);
+    ind1 = find(sample > 0.5); ind2 = find(sample <= 0.5);
+    sample(ind1) = 1; sample(ind2) = 0;
+    sample = double(sample);
 
-figure, plot3D(squeeze(sample)); axis on; grid on;
+    figure, plot3D(squeeze(sample)); axis on; grid on;
+end
 
 %% show reconstruction results of VAE
-% clear; close all; clc;
-% 
-% re_file = '/home/yltian/3D/Code/My3DCode/lua/3Dgeneration_VAE.h5';
-% or_file = '/home/yltian/3D/Data/val_HDF5/04379243.h5';
-% 
-% re_data = h5read(re_file, '/data');
-% re_data = permute(re_data, [5, 4, 3, 2, 1]);
-% re_data = squeeze(re_data);
-% re_data = re_data(:,2:31,2:31,2:31);
-% 
-% or_data = h5read(or_file, '/data');
-% or_data = permute(or_data, [4, 3, 2, 1]);
-% 
-% for i = 1:size(or_data,1)
-%     sample1 = or_data(i,:,:,:);
-%     sample1 = squeeze(sample1);
-%     figure, plot3D(squeeze(sample1)); axis on; grid on;
-%     print(['./pictures/' num2str(i) '_original'], '-dpng');
-%     close;
-%     
-%     temp = 1;
-%     
-%     sample2 = re_data(i,:,:,:);
-%     sample2 = squeeze(sample2);
-%     ind1 = find(sample2 > 0.5); ind2 = find(sample2 <= 0.5);
-%     sample2(ind1) = 1; sample2(ind2) = 0;
-%     figure, plot3D(squeeze(sample2)); axis on; grid on;
-%     print(['./pictures/' num2str(i) '_recover'], '-dpng');
-%     close;
-%     
-%     temp = temp + 1;
-% end
+clear; close all; clc;
+
+re_file = '/home/yltian/3D/Code/My3DCode/lua/3Dgeneration_VAE.h5';
+or_file = '/home/yltian/3D/Data/val_HDF5/04379243.h5';
+
+re_data = h5read(re_file, '/data');
+re_data = permute(re_data, [5, 4, 3, 2, 1]);
+re_data = squeeze(re_data);
+re_data = re_data(:,2:31,2:31,2:31);
+
+or_data = h5read(or_file, '/data');
+or_data = permute(or_data, [4, 3, 2, 1]);
+
+for i = 1:size(or_data,1)
+    sample1 = or_data(i,:,:,:);
+    sample1 = squeeze(sample1);
+    figure, plot3D(squeeze(sample1)); axis on; grid on;
+    print(['./pictures/' num2str(i) '_original'], '-dpng');
+    close;
+    
+    temp = 1;
+    
+    sample2 = re_data(i,:,:,:);
+    sample2 = squeeze(sample2);
+    ind1 = find(sample2 > 0.5); ind2 = find(sample2 <= 0.5);
+    sample2(ind1) = 1; sample2(ind2) = 0;
+    figure, plot3D(squeeze(sample2)); axis on; grid on;
+    print(['./pictures/' num2str(i) '_recover'], '-dpng');
+    close;
+    
+    temp = 1;
+    
+    sample3 = re_data(i+128,:,:,:);
+    sample3 = squeeze(sample3);
+    ind3 = find(sample3 > 0.5); ind4 = find(sample3 <= 0.5);
+    sample3(ind3) = 1; sample3(ind4) = 0;
+    figure, plot3D(squeeze(sample3)); axis on; grid on;
+    print(['./pictures/' num2str(i) '_recover_true'], '-dpng');
+    close;
+    
+    temp = 1;
+end
